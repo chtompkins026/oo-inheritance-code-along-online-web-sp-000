e5c7f15070be0a57193e7adf287ab9aad2e734c9
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-25 23:27:53 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -5,4 +5,7 @@ class Car < Vehicle
   def go 
     return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
   end 
+  
 end
+
+#first looks within the class to which the instance of the car that we are calling the method on belongs to 
